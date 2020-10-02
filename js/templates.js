56fdec154ea7a413d84845d0af38873e6abe6a8d
CGill98 <70035932+CGill98@users.noreply.github.com>
2020-10-02 23:10:58 +0100

    Add files via upload

--- js/templates.js
+++ js/templates.js
@@ -4,15 +4,30 @@ let main_content_inside = ""
 let home_prepend = "../"
 let pages_prepend = ""
 
+let proj_link_class = { //project link classes
+    index : "''",
+    plan_and_do: "''",
+    plan_and_do_privacy: "''",
+
+}
+
 
 switch (page_id) {
     case "index":
         home_prepend = ""
         pages_prepend = "./pages/"
+        proj_link_class.index = "selected-link"
         break;
     case "plan-and-do":
+        proj_link_class.plan_and_do = '"selected-link"'
+        main_content_inside = 
+        `
+        <h2>Plan and Do <br> A React Native To-Do List App</h2>
+        <img class="screenshot" src="../assets/images/screen.png" width="200">
+        `
         break;
     default: //plan-and-do-privacy
+        proj_link_class.plan_and_do_privacy = "selected-link"
         main_content_inside = 
             `
             <div>
@@ -35,8 +50,8 @@ const header_wrapper =
     <div class="header-container">
         <h1>Cooper Gill,<br> Mobile and Web Developer</h1>
         <ul class="project-list">
-            <li><a href="${home_prepend}index.html">Home</a></li>
-            <li><a href="${pages_prepend}plan-and-do.html">Plan and Do</a></li>
+            <li><a class=${proj_link_class.index} href="${home_prepend}index.html">Home</a></li>
+            <li><a class=${proj_link_class.plan_and_do} href="${pages_prepend}plan-and-do.html">Plan and Do</a></li>
             <li><a href="#">Plan and Do</a></li>
             <li class="project-list-last"><a href="#">Plan and Do</a></li>
         </ul>
@@ -57,7 +72,7 @@ const footer_wrapper =
 <div class="footer-wrapper">
     <div class="footer-container">
         <ul class="footer-list">
-            <li><a href="${pages_prepend}plan-and-do-privacy.html">Plan and Do Privacy Policy</a></li>
+            <li><a class=${proj_link_class.plan_and_do_privacy} href="${pages_prepend}plan-and-do-privacy.html">Plan and Do Privacy Policy</a></li>
         </ul>
     </div>
 </div>
